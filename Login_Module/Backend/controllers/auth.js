012b40d6b3f61535d00e8873fa95ad3246914802
Ayush-Dudhe <89478748+Ayush-Dudhe@users.noreply.github.com>
2021-11-23 12:40:44 +0530

    Delete Login_Module directory

--- Login_Module/Backend/controllers/auth.js
+++
@@ -1,3 +0,0 @@
-const { OAuth2Client } = require('google-auth-library');
-const { response } = require('express');
-const client = new OAuth2Client("881458096689-beugkqj0c83vf7qspd114bgj979hsq4f.apps.googleusercontent.com")
